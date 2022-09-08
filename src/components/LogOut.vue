37c2313d0049cf627492bdc2d9ecc35c97ee9595
Shau00n <flankeeeer7@gmail.com>
2022-09-09 00:02:24 +0900

    colorをbasilに変更

--- src/components/LogOut.vue
+++ src/components/LogOut.vue
@@ -1,37 +1,37 @@
 <template>
-<MyBar />
-<div class="text-center">
+  <MyBar />
+  <div class="text-center">
     <div class="my-15">
-    <v-sheet
-      class="px-7 pt-7 pb-4 mx-auto text-center d-inline-block"
-      color="indigo"
-      height="300"
-      outlined
-    >
-      <div class="grey--text text--lighten-1 text-body-2 mb-4">
-        Do you want to logout?
-      </div>
-
-      <v-btn
-        :disabled="loading"
-        class="ma-1 my-15"
-        color="white"
-        plain
-        @click="re"
+      <v-sheet
+        class="px-7 pt-7 pb-4 mx-auto text-center d-inline-block"
+        color="basil"
+        height="300"
+        outlined
       >
-        Cancel
-      </v-btn>
+        <div class="grey--text text--lighten-1 text-body-2 mb-4">
+          Do you want to logout?
+        </div>
 
-      <v-btn
-        :loading="loading"
-        class="ma-1"
-        color="red"
-        plain
-        @click="remove"
-      >
-        Delete
-      </v-btn>
-    </v-sheet>
+        <v-btn
+          :disabled="loading"
+          class="ma-1 my-15"
+          color="white"
+          plain
+          @click="re"
+        >
+          Cancel
+        </v-btn>
+
+        <v-btn
+          :loading="loading"
+          class="ma-1"
+          color="red"
+          plain
+          @click="remove"
+        >
+          Delete
+        </v-btn>
+      </v-sheet>
     </div>
   </div>
 </template>
@@ -40,32 +40,34 @@
 import MyBar from "./MyBar.vue";
 export default {
   data: () => ({
-       loading: false 
-      
-    }),
+    loading: false,
+  }),
   components: {
     MyBar: MyBar,
   },
   methods: {
-      async remove () {
-        this.loading = true
+    async remove() {
+      this.loading = true;
 
-        await new Promise(resolve => setTimeout(resolve, 3000))
+      await new Promise((resolve) => setTimeout(resolve, 3000));
 
-        this.loading = false
+      this.loading = false;
 
-        this.$router.push('/home');
-      },
-
-      re(){
-          this.$router.push('/timelab');
-      }
+      this.$router.push("/home");
     },
+
+    re() {
+      this.$router.push("/timelab");
+    },
+  },
 };
 </script>
  <style>
- /* .text-center{
-     margin: auto;
-     margin-top: 100px;
- } */
- </style>>
+/* Helper classes */
+.basil {
+  background-color: #fffbe6 !important;
+}
+.basil--text {
+  color: #356859 !important;
+}
+</style>
